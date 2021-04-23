1429b515bec22faaef1eea71b258717038e0aa6d
fedya-at <73789328+fedya-at@users.noreply.github.com>
2021-04-24 00:09:26 +0200

    Add files via upload

---
+++ atelierTask/atelierTask/LongRun.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace atelierTask
+{
+  
+    class LongRun
+    {
+        int x;
+        int y;
+        long s;
+        public LongRun()
+        {
+            x = 0;
+            y = 0;
+            s = 0;
+
+        }
+        private long dummyFunction()
+        {
+
+            for (int x = 0; x < 10000; x++)
+            {
+                for (int y = 0; y < 100000; y++)
+                {
+                    s++;
+                }
+            }
+            return s;
+        }
+        private long dummyFunction(CancellationToken cancel)
+        {
+            long s = 0;
+            for (int x = 0; x < 10000; x++)
+            {
+                for (int y = 0; y < 100000; y++)
+                {
+                    if (cancel.IsCancellationRequested)
+                    {
+                        return s;
+                    }
+
+                }
+                s++;
+            }
+
+            return s;
+        }
+        public long count()
+        {
+            for (int x = 0; x < 10000; x++)
+            {
+                for (int y = 0; y < 100000; y++)
+                {
+                    s++;
+                }
+            }
+            return s;
+        }
+        public async Task<long> CountAsync()
+        {
+            return await Task.Run<long>(() =>
+            {
+                for (int x = 0; x < 1000000; x++)
+                {
+                    for (int y = 0; y < 10000000; y++)
+                    {
+                        s++;
+                    }
+                }
+                return s;
+
+
+            }
+
+            );
+        }
+        public async Task<long> CountAsync2()
+        {
+            return await Task.Run<long>(() => dummyFunction());
+        }
+        public async Task<long> CountAsync3()
+        {
+            
+            return await Task.Run<long>(() => dummyFunction());
+        }
+        public async Task<long> CountAsync4(CancellationToken ct)
+        {
+            try
+            {
+                return await Task.Run<long>(() => dummyFunction(ct), ct);
+
+            }
+            catch (OperationCanceledException e)
+            {
+                throw e;
+
+            }
+
+        }
+
+    
+}
+}
---
+++ atelierTask/atelierTask/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace atelierTask
+{
+
+    public partial class MainWindow : Window
+    {
+        CancellationTokenSource tokenSource;
+        CancellationToken token;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wResult.Text = "";
+            Wfin.Visibility = Visibility.Collapsed;
+
+            rectangle.Visibility = System.Windows.Visibility.Visible;
+            LongRun lr = new LongRun();
+            wResult.Text = lr.count().ToString();
+            rectangle.Visibility = System.Windows.Visibility.Collapsed;
+            Wfin.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tokenSource.Cancel();
+                rectangle.Visibility = System.Windows.Visibility.Visible;
+                Wfin.Visibility = Visibility.Collapsed;
+                wResult.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error d'annulation");
+            }
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            wResult.Text = "";
+            Wfin.Visibility = Visibility.Collapsed;
+
+            rectangle.Visibility = System.Windows.Visibility.Visible;
+            LongRun lr = new LongRun();
+            long i = await lr.CountAsync3();
+            wResult.Text = i.ToString();
+            rectangle.Visibility = System.Windows.Visibility.Collapsed;
+
+            Wfin.Visibility = Visibility.Visible;
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            wResult.Text = "";
+            Wfin.Visibility = Visibility.Collapsed;
+            rectangle.Visibility = System.Windows.Visibility.Visible;
+            tokenSource = new CancellationTokenSource();
+            try
+            {
+                LongRun lr = new LongRun();
+                long i = await lr.CountAsync4(token);
+                wResult.Text = i.ToString();
+            }catch (TaskCanceledException)
+            {
+                MessageBox.Show("Canceled");
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("canceled");
+            }
+            finally
+            {
+                tokenSource.Dispose();
+                rectangle.Visibility = System.Windows.Visibility.Visible;
+                Wfin.Visibility = Visibility.Visible;
+            }
+
+        }
+
+        private void btnGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello , it works !!");
+        }
+
+       
+    }
+}
